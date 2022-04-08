2efaae14fdea04f38462520d66cad0bcbd2b0edb
d00438530 <103125971+d00438530@users.noreply.github.com>
2022-04-08 08:34:43 -0600

    ADDED CODE

--- main.ts
+++ main.ts
@@ -1,4 +1,27 @@
-let potato = 0
 input.onButtonPressed(Button.A, function () {
-    potato = randint(2, 9)
+    basic.clearScreen()
+    basic.showString("3 2 1 GO!")
+    potato = randint(5, 15)
+    while (potato > 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        potato += -1
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+    basic.showIcon(IconNames.Skull)
 })
+let potato = 0
+basic.showString("A TO START")
