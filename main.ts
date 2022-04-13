267a11d4f6a977de1e798157bfeccccb14ec6bc6
d00438530 <103125971+d00438530@users.noreply.github.com>
2022-04-13 08:56:36 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString("3 2 1 GO!")
-    potato = randint(5, 15)
+    range1.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(500)
+    range22.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(500)
+    range3.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(500)
+    range4.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(500)
+    potato = randint(3, 10)
     while (potato > 0) {
         basic.showLeds(`
             . . . . .
@@ -10,6 +18,7 @@ input.onButtonPressed(Button.A, function () {
             . . # . .
             . . . . .
             `)
+        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
         basic.pause(500)
         potato += -1
         basic.showLeds(`
@@ -19,14 +28,34 @@ input.onButtonPressed(Button.A, function () {
             . # . # .
             . . # . .
             `)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
         basic.pause(500)
     }
     basic.showIcon(IconNames.Skull)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
 let potato = 0
-basic.showString("A TO START")
-let strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
+let range4: neopixel.Strip = null
+let range3: neopixel.Strip = null
+let range22: neopixel.Strip = null
+let range1: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
 let range = strip.range(0, 7)
-let rangne2 = strip.range(7, 14)
+let range2 = strip.range(7, 14)
+range1 = strip.range(0, 4)
+range22 = strip.range(4, 7)
+range3 = strip.range(7, 10)
+range4 = strip.range(10, 14)
 range.showColor(neopixel.rgb(randint(0, 65), randint(0, 65), randint(0, 65)))
-rangne2.showColor(neopixel.rgb(randint(0, 65), randint(0, 65), randint(0, 65)))
+range2.showColor(neopixel.rgb(randint(0, 65), randint(0, 65), randint(0, 65)))
+basic.showString("B TO START")
+strip.showRainbow(1, 360)
